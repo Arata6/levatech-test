da090c2912df437b323f42137a3cead1c7e44f26
Arat6 <24w6045d@shinshu-u.ac.jp>
2024-06-27 06:23:54 +0900

    課題2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,21 +5,20 @@
         <title>Blog</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
 
     <body class="antialiased">
         <h1>Blog Name</h1>
         <div class ='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class ='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body}}</p>
-                </div>
+            @foreach($posts as $post)
+            <div class='post'>
+                <h2 class ='title'>{{ $post->title }}</h2>
+                <p class='body'>{{ $post->title }} </p>
+            </div>
             @endforeach
         </div>
-        <div class ='paginate'>
+        <div class='paginate'>
             {{ $posts->links() }}
         </div>
     </body>
