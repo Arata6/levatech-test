9f25bb973122f517c2ebe636303127d24baf86ae
Arat6 <24w6045d@shinshu-u.ac.jp>
2024-06-28 07:44:07 +0900

    basis03

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,18 +3,18 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
 
-    <body class="antialiased">
+    <body>
         <h1>Blog Name</h1>
         <div class ='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    
+                    <h2 class ='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
                     <p class='body'>{{ $post->body}}</p>
                 </div>
             @endforeach
